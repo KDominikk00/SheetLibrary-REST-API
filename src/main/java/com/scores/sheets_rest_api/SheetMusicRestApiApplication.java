a813b53caef7ac61986ac0a7c145b305803ad315
KDominikk00 <dominikkowalski331@gmail.com>
2024-07-06 22:25:47 +0000

    Initial commit

---
+++ src/main/java/com/scores/sheets_rest_api/SheetMusicRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.scores.sheets_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheetMusicRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SheetMusicRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scores/sheets_rest_api/SheetMusicRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scores.sheets_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheetMusicRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
